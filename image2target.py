7a05dd48f44ebaca92ad6891be61d664466b7276
satlawa <p.satlawa@gmx.net>
2019-11-22 10:38:42 +0100

    implementation of image-pixel to baseframe-meter coordinates for joints

--- image2target.py
+++ image2target.py
@@ -50,6 +50,9 @@ class image2target:
         self.pix2meter0 = 0
         self.pix2meter1 = 0
 
+        self.base_pix_coord0 = np.array([0,0])
+        self.base_pix_coord1 = np.array([0,0])
+
     def detect_joints(self, img):
         joints = []
         for colour in ["red", "green", "blue", "yellow"]:
@@ -66,10 +69,32 @@ class image2target:
         boundries, contours = self.od.find_boundries(img)
         return img, boundries, contours
 
+    def classify_target(self, img, boundries):
+        # classify target object | 0 = rectangle | 1 = circle
+        obj = self.od.get_object(img, boundries)
+        prediction = self.svm.classify(obj)
+        return prediction
+
     def set_pix2meter(self, joints0, joints1):
         # set conversion factor (pixels to meters) between green and blue joints (3 meters)
         self.pix2meter0 = self.coord.calc_pixel_to_meter_2D(joints0[1], joints0[2], 3)
         self.pix2meter1 = self.coord.calc_pixel_to_meter_2D(joints1[1], joints1[2], 3)
+
+    def set_base_coordinates(self, joints0, joints1):
+        # set base pixel coordinates
+        self.base_pix_coord0 = joints0[0]
+        self.base_pix_coord1 = joints1[0]
+
+    def convert_coordinates(self, joints0, joints1):
+        # convert image-pixel coordinates to base-merter coordinates
+        joint_base0 = []
+        for joint in joints0:
+            joint_base0.append((joint - self.base_pix_coord0) * self.pix2meter0)
+        joint_base1 = []
+        for joint in joints1:
+            joint_base1.append((joint - self.base_pix_coord1) * self.pix2meter1)
+
+        return np.array(joint_base0), np.array(joint_base1)
 
     def merge_coordinates(self, joints0, joints1):
         coordinates = []
@@ -87,25 +112,24 @@ class image2target:
             self.cv_image1 = self.bridge.imgmsg_to_cv2(data1, "bgr8")
         except CvBridgeError as e:
             print(e)
-        # Uncomment if you want to save the image
-        #if self.iterator % 50 == 0:
-        #    print(self.iterator)
-        #    cv2.imwrite('image_1_'+str(self.iterator/50)+'.png', self.cv_image1)
-        # show images
-        #im1=cv2.imshow('window1', self.cv_image0)
-        #im2=cv2.imshow('window2', self.cv_image1)
-        #cv2.waitKey(1)
 
-        ##############
-        ### joints ###
-        ##############
+        ######################################################################
+        ###                         joints                                 ###
+        ######################################################################
 
         # detect joints from camera1 ans camera2
         joints0 = self.detect_joints(self.cv_image0)
         joints1 = self.detect_joints(self.cv_image1)
 
-        # set conversion factor between green and blue joints
-        self.set_pix2meter(joints0, joints1)
+
+        if self.pix2meter0 == 0:
+            # set conversion factor between green and blue joints
+            self.set_pix2meter(joints0, joints1)
+            # set base pixel coordinates
+            self.set_base_coordinates(joints0, joints1)
+
+        # convert image-pixel coordinates to base-merter coordinates
+        joints0, joints1 = self.convert_coordinates(joints0, joints1)
 
         # merge pixel coordinates from both images to coordinates x,y,z
         coordinates = self.merge_coordinates(joints0, joints1)
@@ -113,13 +137,11 @@ class image2target:
         # calculate angles
         #print(self.coord.calc_angles_joints_3D(coordinates[1], coordinates[2]))
 
-        #joints0 = joints0 * self.pix2meter0
-        #joints0 = joints0.round(0)
-        #print(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
+        print(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
 
-        ###############
-        ### targets ###
-        ###############
+        ######################################################################
+        ###                         targets                                ###
+        ######################################################################
 
         # get filterd & thresholded image, boundries, contours of target objects
         img, boundries, contours = self.detect_target(self.cv_image0)
@@ -144,10 +166,10 @@ class image2target:
             #self.targets.data = np.array([0, 0, 0, 0])
             data1 = [0, 0, 0]
 
-        print("target 1 ", data0, " target 2 ", data1)
+        #print("target 1 ", data0, " target 2 ", data1)
 
 
-        # set variables for ublishing
+        # set variables for publishing
         self.target = Float64MultiArray()
         self.target.data = np.array([data0, data1])
         # Publish the results
